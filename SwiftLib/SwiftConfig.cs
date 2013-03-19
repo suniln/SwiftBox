b193c7e9a3ff812aaaf67c72fdbca5fd1b94ee09
Sunil Nayak <sunilnayaknf@yahoo.com>
2013-03-18 22:45:33 -0700

    Protptype c# implementation of OpenStack Object Storage API integration
    This is a sample c# implementation of OpenStack Object Storage (swift)
    API integration. Uses RestSharp REST client (http://restsharp.org/).
    Swift API doc:
    http://docs.openstack.org/api/openstack-object-storage/1.0/content/

---
+++ SwiftLib/SwiftConfig.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SwiftLib
+{
+    public class SwiftConfig
+    {
+        private string url;
+
+        public string Url
+        {
+            get { return url; }
+            set { url = value; }
+        }
+        private string user;
+
+        public string User
+        {
+            get { return user; }
+            set { user = value; }
+        }
+        private string authkey;
+
+        public string Authkey
+        {
+            get { return authkey; }
+            set { authkey = value; }
+        }
+
+        private string boxFolder;
+
+        public string BoxFolder
+        {
+            get { return boxFolder; }
+            set { boxFolder = value; }
+        }
+
+        private string downloadFolder;
+
+        public string DownloadFolder
+        {
+            get { return downloadFolder; }
+            set { downloadFolder = value; }
+        }
+
+    }
+}
